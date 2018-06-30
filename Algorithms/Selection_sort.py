b708c4ee57cd6b49943c6716e5bc0aefcf10b6d4
Anthony Perez <stockcart21@yahoo.com>
2018-06-29 17:18:44 -0700

    Create Selection_sort.py
    During each iteration the algorithm is taking small elements and swapping out the little elements and replacing it with the larger inputs.

---
+++ Algorithms/Selection_sort.py
@@ -0,0 +1,11 @@
+# Selection sorting is a alogorithm that finds a small element and switches it within the array
+
+#Implementation
+
+def selection_sort(arr):
+   for k in range(len(arr):
+     min_index = k # smallest elements
+     for j in range(k+1, len(arr)):
+       if arr[min_index] > arr[j]:
+         min_index = j
+       arr[k], arr[min_index] = arr[min_index], arr[k]
