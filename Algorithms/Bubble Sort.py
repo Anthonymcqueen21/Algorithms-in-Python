f35347828dcbe1fb44f475a97214410560450491
Anthony Perez <stockcart21@yahoo.com>
2018-06-29 17:13:31 -0700

    Create Bubble Sort.py
    Bubble sorting is used to sort a list of elements comparing adjacent elements

---
+++ Algorithms/Bubble Sort.py
@@ -0,0 +1,10 @@
+# Bubble sorting is an algorithm which is used to sort a list of elements in arrays.
+
+# Implementation
+
+def bubble_sort(nlist):
+   for passnum in
+range(len(nlist)-1,0,-1):
+   for j in range(passnum):
+      if nlist[j]>nlist[j+1]:
+        nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
