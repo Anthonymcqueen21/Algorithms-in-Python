689c6c020d8c8d85d13999a2170e0379bfa23185
Anthony Perez <stockcart21@yahoo.com>
2018-06-29 18:05:25 -0700

    Create Quick Sort.py
    The quicksort algorithm functions as a pivot of elements using recursive.

---
+++ Divide and Conquer/Quick Sort.py
@@ -0,0 +1,17 @@
+# Again another algorithm that breaks down problems into smaller problems.
+
+# Implementation
+
+from random import randint
+
+def quicksort(lst, start, end):
+   if start < end:
+      pivot = randint(start, end)
+      # swap with the last element
+lst[end],lst[pivot] = lst[pivot],lst[end]
+     # partition the list
+     split = partition(lst, start, end)
+     # sort both halves
+     quicksort(lst, start, split-1)
+     quicksort(lst, split+1, end)
+      
