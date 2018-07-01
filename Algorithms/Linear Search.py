840b07b767bdd10913a28c6f9e7db631bc0c1ec0
Anthony Perez <stockcart21@yahoo.com>
2018-07-01 16:52:37 -0700

    Create Linear Search.py
    This checks the element and returns the index

---
+++ Algorithms/Linear Search.py
@@ -0,0 +1,9 @@
+# Linear search is a algorithm that searches for a missing element
+
+#Implementation 
+
+def search(x, my_list):
+   for k in range(len(my_list)):
+     if my_list[k] == x:
+       return k
+     return - 1
