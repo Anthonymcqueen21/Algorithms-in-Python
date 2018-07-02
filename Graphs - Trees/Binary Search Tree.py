4e389302e97fb38fa2a4deced54b78c4fc11113c
Anthony Perez <stockcart21@yahoo.com>
2018-07-01 17:18:47 -0700

    Create Binary Search Tree.py
    This binary search tree that keys each node that is greater than or equal to the key stored in the left sub-tree.

---
+++ Graphs - Trees/Binary Search Tree.py
@@ -0,0 +1,13 @@
+# This is a algorithm that finds where the value is less than or equal to.
+
+# Implementation
+def search(root, data):
+   if root is None:
+      print('Not Found')
+   if root.data == data:
+      print('Found at:', hex(id(hoot)))
+      return root
+   if root.data < data:
+      return search(root.right,data)
+   else:
+      return search(root.left, data)
