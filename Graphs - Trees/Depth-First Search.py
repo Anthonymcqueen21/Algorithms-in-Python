34dfcef835506a281941774589b250a0fe712eba
Anthony Perez <stockcart21@yahoo.com>
2018-07-01 17:14:50 -0700

    Create Depth-First Search.py
    This is a search algorithm used for trees and graphs and searches for the list of vertices and points them in the right direction.

---
+++ Graphs - Trees/Depth-First Search.py
@@ -0,0 +1,11 @@
+# This algorithm is designed for graphs and trees
+
+# Implementation
+def dfs(graph, vertex, path=[]):
+   path += [vertex]
+   
+   for neighbor in graph[vertex]:
+      if neighbor no in path:
+         path = dfs(graph, neighbor, path)
+         
+   return path
