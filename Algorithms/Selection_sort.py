18c72d79681ef373540538b8f760d37fca2ac78a
Anthony Perez <stockcart21@yahoo.com>
2018-07-21 21:37:50 -0700

    Update Selection_sort.py

--- Algorithms/Selection_sort.py
+++ Algorithms/Selection_sort.py
@@ -5,7 +5,7 @@
 def selection_sort(arr):
    for k in range(len(arr):
      min_index = k # smallest elements
-     for j in range(k+1, len(arr)):
+   for j in range(k+1, len(arr)):
        if arr[min_index] > arr[j]:
          min_index = j
-       arr[k], arr[min_index] = arr[min_index], arr[k]
+   arr[k], arr[min_index] = arr[min_index], arr[k]
