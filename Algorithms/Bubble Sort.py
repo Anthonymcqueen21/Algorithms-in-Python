dc9ae7b883548955667bb1935b01a351a56cacb3
Anthony Perez <stockcart21@yahoo.com>
2018-07-25 20:15:09 -0700

    Update Bubble Sort.py

--- Algorithms/Bubble Sort.py
+++ Algorithms/Bubble Sort.py
@@ -3,7 +3,7 @@
 # Implementation
 def bubble_sort(nlist):
    for passnum in
-range(len(nlist)-1,0,-1):
+  range(len(nlist)-1,0,-1):
    for j in range(passnum):
       if nlist[j]>nlist[j+1]:
         nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
