26996d6f84f7510dd14c40c7ed5f6381f1fa3680
Anthony Perez <stockcart21@yahoo.com>
2018-08-02 19:08:00 -0700

    Update Depth-First Search.py

--- Graphs - Trees/Depth-First Search.py
+++ Graphs - Trees/Depth-First Search.py
@@ -3,7 +3,6 @@
 # Implementation
 def dfs(graph, vertex, path=[]):
    path += [vertex]
-   
    for neighbor in graph[vertex]:
       if neighbor no in path:
          path = dfs(graph, neighbor, path)
