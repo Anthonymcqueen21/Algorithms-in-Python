4cde8b97e73ba364aec56d0977b39a8d9fa8f0b1
Anthony Perez <stockcart21@yahoo.com>
2018-08-04 22:13:20 -0700

    Update Insertion_sort.py

--- Algorithms/Insertion_sort.py
+++ Algorithms/Insertion_sort.py
@@ -7,6 +7,6 @@ def insertion_sort(arr):
      key = arr[k]
      j = k
      while j > 0 and arr[j-1]
-  > arr[j]:
-       arr[j], arr[j-1] = arr[j-1], arr[j]
+  arr[j]:
+     arr[j], arr[j-1] = arr[j-1], arr[j]
           j = j - 1
