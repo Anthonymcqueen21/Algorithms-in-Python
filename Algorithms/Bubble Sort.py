ae8e871db3661566dc5b4d99d7a2a659f85deadb
Anthony Perez <stockcart21@yahoo.com>
2018-09-12 14:35:43 -0700

    Update Bubble Sort.py

--- Algorithms/Bubble Sort.py
+++ Algorithms/Bubble Sort.py
@@ -4,7 +4,7 @@
 # Python Script
 def bubble_sort(nlist):
    for passnum in
-range(len(nlist)-1,0,-1):
+for i in range(len(nlist)-1,0,-1):
    for j in range(passnum):
       if nlist[j]>nlist[j+1]:
    nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
