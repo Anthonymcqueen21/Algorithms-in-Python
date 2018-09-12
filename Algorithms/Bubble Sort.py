02c9017de047d9256c2eda2e37d0e67cdbd2aa3c
Anthony Perez <stockcart21@yahoo.com>
2018-09-12 14:35:14 -0700

    Update Bubble Sort.py

--- Algorithms/Bubble Sort.py
+++ Algorithms/Bubble Sort.py
@@ -8,3 +8,26 @@ range(len(nlist)-1,0,-1):
    for j in range(passnum):
       if nlist[j]>nlist[j+1]:
    nlist[j], nlist[j+1] = nlist[j+1], nlist[j]
+
+   
+# Defining num_swaps and count_swaps in a problem statement
+
+def countSwaps(lst):
+    num_swaps = 0
+    if len(lst) < 2:
+        return num_swaps
+    for i in range(len(lst) - 1, 0, -1):
+        for j in range(i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j+1] = lst[j+1],lst[j]
+                num_swaps += 1
+return num_swaps
+
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+      
+swaps = countSwaps(a)
+      
+print('Array is sorted in {0} swaps.'.format(swaps))
+print('First Element: {0}'.format(a[0]))
+print('Last Element: {0}'.format(a[-1]))
