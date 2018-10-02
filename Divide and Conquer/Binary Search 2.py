89fe3e203a8667a04229da123da3bf44a8d01e46
Anthony Perez <stockcart21@yahoo.com>
2018-10-01 20:00:35 -0700

    Create Binary Search 2.py
    Time Complexity 0(lg n)

---
+++ Divide and Conquer/Binary Search 2.py
@@ -0,0 +1,20 @@
+def search(seq, key):
+    """ Takes a list of integers and searches if the 'key' is contained within the list.
+    
+    :param seq: A list of integers
+    :param key: The integer to be searched for 
+    :rtype: The index of where the 'key' is located in the list. If 'key' is
+              not found then False is returned.
+    """
+    
+    lo = 0
+    hi = len(seq) - 1
+    while hi >= lo:
+       mid = lo + (hi - lo) // 2
+       if seq[mid] < key:
+          lo = mid + 1
+       elif seq[mid] > key:
+          hi = mid - 1
+       else:
+          return mid
+    return False
